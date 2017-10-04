c206f580e15a8598b70192bb9041e88b4491de52
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-10-04 20:18:16 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/**
+ * @param {Array} versions
+ * @param {Color} [startColor]
+ * @param {Color} [endColor]
+ * @return {Array} colors by semver
+ */
+module.exports = function(versions, startColor, endColor) {
+    // TODO implement it :)
+};
